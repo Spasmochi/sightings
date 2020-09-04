a0156ce9761b8e48609a1fd1589c2dd887e59dbf
Spas <12554483+Spasmochi@users.noreply.github.com>
2020-09-04 20:57:54 +1000

    added much logic

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,18 @@ import {
 } from "@react-google-maps/api";
 import { formatRelative } from "date-fns";
 import mapStyle from "./mapStyle";
+import { useState, useCallback } from "react";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxList,
+  ComboboxOption,
+  ComboboxPopover,
+} from "@reach/combobox";
 
 const libraries = ["places"];
 const mapContainerStyle = {
@@ -30,6 +42,25 @@ export default function Home() {
     libraries,
   });
 
+  const [markers, setMarkers] = useState([]);
+  const [Selected, setSelected] = useState(null);
+
+  const onMapClick = useCallback((event) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  const mapRef = React.useRef();
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
   if (loadError) return "Error loading maps";
   if (!isLoaded) return "Loading maps";
 
@@ -41,24 +72,89 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1>Ghosts &#x1F47B;</h1>
+        <h1 className={styles.h1}>Ghosts &#x1F47B;</h1>
+        <Search />
         <GoogleMap
           mapContainerStyle={mapContainerStyle}
           zoom={15}
           center={center}
           options={options}
-        ></GoogleMap>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://github.com/Spasmochi"
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={onMapClick}
+          onLoad={onMapLoad}
         >
-          A Spasmochi Creation{" "}
-        </a>
-      </footer>
+          {markers.map((marker) => (
+            <Marker
+              key={marker.time.toISOString()}
+              position={{ lat: marker.lat, lng: marker.lng }}
+              icon={{
+                url: "/ghost.svg",
+                scaledSize: new window.google.maps.Size(30, 30),
+                origin: new window.google.maps.Point(0, 0),
+                anchor: new window.google.maps.Point(15, 15),
+              }}
+              onClick={() => {
+                setSelected(marker);
+              }}
+            />
+          ))}
+          {Selected ? (
+            <InfoWindow
+              position={{ lat: Selected.lat, lng: Selected.lng }}
+              onCloseClick={() => {
+                setSelected(null);
+              }}
+            >
+              <div>
+                <h2>Ghost Seen</h2>
+                <p>{formatRelative(Selected.time, new Date())}</p>
+              </div>
+            </InfoWindow>
+          ) : null}
+        </GoogleMap>
+      </main>
+    </div>
+  );
+}
+
+function Search() {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({});
+
+  return (
+    <div className={styles.comboBox}>
+      <Combobox
+        onSelect={async (address) => {
+          try {
+            const results = await getGeocode({ address });
+            console.log(results[0]);
+          } catch (error) {
+            console.log(error);
+          }
+
+          console.log(address);
+        }}
+      >
+        <ComboboxInput
+          className={styles.comboInput}
+          value={value}
+          onChange={(event) => {
+            setValue(event.target.value);
+          }}
+          disabled={!ready}
+          placeholder="Find a location"
+        />
+        <ComboboxPopover>
+          {status === "OK" &&
+            data.map(({ id, description }) => (
+              <ComboboxOption value={description} />
+            ))}
+        </ComboboxPopover>
+      </Combobox>
     </div>
   );
 }
