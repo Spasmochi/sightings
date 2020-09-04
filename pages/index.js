9c2d1a45636ef3da56189f060971db70bd3ec087
Spas <12554483+Spasmochi@users.noreply.github.com>
2020-09-05 09:52:56 +1000

    style element added

--- pages/index.js
+++ pages/index.js
@@ -77,7 +77,11 @@ export default function Home() {
           `}
         </style>
       </Head>
-
+      <style jsx>
+        {`
+          font-family: proxima-nova, sans-serif !important;
+        `}
+      </style>
       <main className="relative h-full">
         <Shell panTo={panTo}>
           <GoogleMap
