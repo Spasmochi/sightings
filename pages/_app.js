f5f7eee791f32b4f0727a0cb4e27750123ccd22a
Spas <12554483+Spasmochi@users.noreply.github.com>
2020-09-04 22:30:07 +1000

    updated fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-import "../styles/globals.css";
-import "../styles/tailwind.css";
 import "@reach/combobox/styles.css";
+import "../styles/tailwind.css";
+import "../styles/globals.css";
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
